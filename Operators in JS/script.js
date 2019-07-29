8a574a9fd904277f27ba8403380321419db97794
Danil Sadovoy <53143990+Sadovoy-Danil@users.noreply.github.com>
2019-07-29 15:49:31 +0300

    Add files via upload

---
+++ Operators in JS/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// let answer = +prompt('Есть ли вам 18?', 'Да');
+
+// console.log(typeof(answer));
+
+// console.log('arr' + ' — object');
+// console.log(4 + ' — object');
+
+let incr = 10, // инкремент — операция увеличивающая переменную на 1.
+    decr = 10; // дикремент — операция уменьшающая переменную на 1.
+
+incr++; // = 10 + 1
+decr++; // = 10 - 1
+
+console.log(5 % 2); // = 1 --> 5 / 2 = 2 + (остаток 1)
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
+
+let isChecked = true,
+    isClosed = false;
+
+console.log(isChecked || isClosed); // && — И, || — ИЛИ
